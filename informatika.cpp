b3f1b3d1f7e672e35ef8cd3affc2681cef1315c4
dashaterentevaa17 <71405685+dashaterentevaa17@users.noreply.github.com>
2020-12-30 10:29:19 +0300

    Add files via upload

---
+++ informatika.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+class TemperatureConverter 
+{
+	
+public: 
+	string convertTemp(int temperature, char convertTo) 
+	{
+		string result = "NONE"; 
+		if (convertTo == 'F' || convertTo == 'f') result = "фаренгейт = " + to_string(9 * (float)temperature / 5 + 32);
+		
+		else if (convertTo == 'C' || convertTo == 'c') result = "Цельсий = " + to_string(((float)temperature - 32) * 5 / 9);
+		return result;
+		
+	}
+};
+
+int main()
+
+{
+	TemperatureConverter type;
+	setlocale(LC_ALL, "ru");
+	cout << type.convertTemp(4, 'F') << endl;
+	cout << type.convertTemp(0, 'C') << endl;
+	cout << type.convertTemp(0, 'd') << endl;
+	system("pause");
+	return 0;
+}
